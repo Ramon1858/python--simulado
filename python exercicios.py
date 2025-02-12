e010c7e68018e239542f146664cebe282374d38f
Ramon1858 <caetanoramon678@gmail.com>
2025-02-12 16:22:42 -0300

    Add files via upload

---
+++ python exercicios.py
@@ -0,0 +1,51 @@
+#nome = input('digite seu nome')
+#print('seja bem vindo(a)',nome)
+
+#info = True
+#print(type(info))
+#n1 = float (input('digite o numero um:'))
+#n2 = float (input('digite o numero dois:'))
+#multiplicação = n1 * n2
+#print(multiplicação)
+
+#NUMERO 4
+#numero1 = int(input('digite um numero:'))
+#numero2 = int(input('digite o numero dois:)'))
+#divisão = numero1/numero2
+#print(divisão)
+
+#NUMERO 5
+#num1 = float(input('write the first number'))
+#num2 = float(input('write down the second number'))
+#subtração = num1-num2
+#print(subtração)
+
+#NUMERO 6
+#numero1 = float(input('digite um numero:'))
+#numero2 = float(input('digite o numero dois:)'))
+#divisão = numero1/numero2
+#print(divisão)
+
+#NUMeRO 7
+#number1 =  float(input('write the first number:'))
+#number2 = float(input('write down the second number:'))
+#multiplicação = number1 * number2
+#print(multiplicação)
+
+#NUMERO 8
+#numero = int(input('numero q sera o dobro'))
+#soma = numero + numero
+#print(soma)
+
+#numero 9
+#numero = float(input('numero q sera o dobro'))
+#soma = numero + numero
+#print(soma)
+
+#numero 10
+
+nome = input('digite seu nome de usuario')
+cumprimento = 'bem vindo(a)'
+print(cumprimento,nome)
+senha = input('digite sua senha')
+print('acesso liberado')
